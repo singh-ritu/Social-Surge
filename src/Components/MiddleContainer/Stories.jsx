f502a7236e7e212d0a951adfa3d7a13e053546ce
Ritu <singhritu241101@gmail.com>
2024-02-15 13:54:13 +0530

    Stories Object file is created

--- src/Components/MiddleContainer/Stories.jsx
+++ src/Components/MiddleContainer/Stories.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import Avatar from "../Avatar";
 import "./stories.styles.css";
+import { stories } from "./constants";
 
 function Stories() {
-  return (
-    <div className="story">
-      <Avatar />
-    </div>
-  );
+  return <div className="story"></div>;
 }
 
 export default Stories;
---
+++ src/Components/MiddleContainer/constants.js
@@ -0,0 +1,45 @@
+import story1 from "../../assets/story1.jpg";
+import story2 from "../../assets/story2.jpg";
+import story3 from "../../assets/story3.jpg";
+import story4 from "../../assets/story4.jpg";
+import story5 from "../../assets/story5.jpg";
+import story6 from "../../assets/story6.jpg";
+import party from "../../assets/party.jpg";
+import birthday from "../../assets/birthday.jpg";
+import friends from "../../assets/party.jpg";
+import college from "../../assets/college.jpg";
+import exams from "../../assets/exams.jpg";
+import wedding from "../../assets/wedding.jpg";
+
+export const stories = [
+  {
+    avatar: story1,
+    username: "varsha",
+    photo: party,
+  },
+  {
+    avatar: story2,
+    username: "rishu_gangwar",
+    photo: birthday,
+  },
+  {
+    avatar: story3,
+    username: "iiabhiskek_kumar",
+    photo: wedding,
+  },
+  {
+    avatar: story4,
+    username: "Mahekkk",
+    photo: friends,
+  },
+  {
+    avatar: story5,
+    username: "akshiiiii",
+    photo: exams,
+  },
+  {
+    avatar: story6,
+    username: "aryyan_rana",
+    photo: college,
+  },
+];
